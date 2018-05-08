0aa4b629a5eb57279e32ba1516f147614fbcc629
choice.qin <choice.qin@nirvana-info.com>
2018-05-08 18:11:47 +0800

    commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,41 @@
+<template>
+  <layout-golden class="screen">
+    <gl-col>
+      <gl-component title="compA" :height="height">
+        <h1>CompA</h1>
+      </gl-component>
+      <gl-component title="compB" :height="height">
+        <h1>CompB</h1>
+      </gl-component>
+      <gl-stack>
+        <gl-component title="compC" :height="height">
+          <h1>CompC</h1>
+        </gl-component>
+      </gl-stack>
+    </gl-col>
+  </layout-golden>
+</template>
+
+<script>
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      height: 10
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+.screen {
+	width: 100vw;
+	height: 100vh;
+}
+body {
+  margin: 0px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,23 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import 'golden-layout/src/css/goldenlayout-dark-theme.css'
+import vgl from 'vue-golden-layout'
+Vue.use(vgl)
+Vue.config.productionTip = false
+const {layoutGolden, glComponent, glRow, glCol, glStack} = {
+  install: function (Vue) {
+    Vue.component('vgl', vgl)
+  }
+}
+export default {layoutGolden, glComponent, glRow, glCol, glStack}
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App, layoutGolden, glComponent, glRow, glCol, glStack },
+  template: '<App/>'
+})
